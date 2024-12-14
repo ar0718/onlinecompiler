758ad0dfb53771580c3982193fed7870ee4625f3
prem12937@gmail.com <prem12937@gmail.com>
2024-12-14 16:24:18 +0530

    feat: add user model

--- compiler/models.py
+++ compiler/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password_hash = models.CharField(max_length=255)
+ 
+    def __str__(self) -> str:
+        return self.username
