9ca69b82895b72c3bfefea6a78be45b6441ba840
prem12937@gmail.com <prem12937@gmail.com>
2024-12-14 16:05:49 +0530

    feat: add login/signup api endpoints

--- compiler/urls.py
+++ compiler/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-    path('', views.Welcome, name='root')
+    path('', views.Welcome, name='root'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login')
 ]
--- compiler/views.py
+++ compiler/views.py
@@ -5,3 +5,13 @@ from rest_framework.response import Response
 def Welcome(request):
     message = {"status_code": 200, "status_message": "api is up", "message": "Hello from AxG!"}
     return Response(message)
+
+@api_view(['POST'])
+def signup(request):
+    message = {"status_code": 200, "status_message": "signup is up", "message": "logic is yet to be implemented"}
+    return Response(message)
+
+@api_view(['POST'])
+def login(request):
+    message = {"status_code": 200, "status_message": "login is up", "message": "logic is yet to be implemented"}
+    return Response(message)
